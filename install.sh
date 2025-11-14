dca3cf673e115324512f0e9b533b63d0eda31d2c
507-TEAM <igrovojkana87@gmail.com>
2025-11-14 18:51:58 +0700

    V1.2

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "Добро пожаловать в установщик 507-Backup!"
+echo "Устанавливаем!"
+
+wget "https://github.com/ffghhjjhkhjkhjkhjkhkj/507-backup/raw/refs/heads/main/binares/507-backup" -O 507-backup
+sudo mv 507-backup /usr/local/bin/
+
+echo "Установка завершена!"
