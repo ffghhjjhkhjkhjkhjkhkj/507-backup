5aa9f56148b892cbe3bc6654ab94b6d52d7285f6
507-TEAM <igrovojkana87@gmail.com>
2025-11-14 18:59:58 +0700

    V1.2

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ echo "Устанавливаем!"
 
 wget "https://github.com/ffghhjjhkhjkhjkhjkhkj/507-backup/raw/refs/heads/main/binares/507-backup" -O 507-backup
 sudo mv 507-backup /usr/local/bin/
+sudo chmod +x /usr/local/bin/507-backup
 
 echo "Установка завершена!"
