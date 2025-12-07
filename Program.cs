3fcd5c6447fdaa8b13da5a3e2c094230c601f506
507-TEAM <igrovojkana87@gmail.com>
2025-12-08 00:53:57 +0700

    V1.3

--- Program.cs
+++ Program.cs
@@ -2,10 +2,16 @@
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
+using System;
 
 Console.WriteLine("Добро пожаловать в 507-BACKUP!");
 Console.WriteLine("Какую папку нужна забекапить?");
 string folder = Console.ReadLine();  // Спрашиваем у пользователя какую папку упаковать
+if (folder == "") {
+    Console.WriteLine("Вы ничего не ввели");
+    Environment.Exit(0);
+}
+// TODO: Добавить проверку на существование папки
 Console.WriteLine("Отлично! приступаем к бекапу...");
 string timestamp = DateTime.Now.ToString("dd.MM.yyyy");
 Directory.CreateDirectory("backup");
